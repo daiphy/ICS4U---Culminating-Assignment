fffdedd1f698a807ddff1328156e5d36c4a74729
blessbtsss <75858309+blessbtsss@users.noreply.github.com>
2022-01-11 21:36:21 -0500

    Add files via upload

---
+++ Finance.java
@@ -0,0 +1,20 @@
+/**
+ * Base Class - Model - Finance
+ */
+
+class Finance {
+    //2D Arrays
+    String[][] antIncomeArr;
+    String[][] accIncomeArr;
+    String[][] accExpenseArr;
+    String[][] antExpenseArr;
+
+    //Methods
+
+    //insert importing method name (file io)
+    //insert exporting method name
+    //insert creating csv name
+    //insert adding and deleting categories method
+    //insert putting arraylist from the categories into 2d array (the customizeable portion)
+
+}
