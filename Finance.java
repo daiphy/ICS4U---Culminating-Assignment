fa2def8527d6b2ffef7a436758b674ca1bef97b8
blessbtsss <75858309+blessbtsss@users.noreply.github.com>
2022-01-12 15:54:06 -0500

    Add files via upload

--- Finance.java
+++ Finance.java
@@ -4,13 +4,22 @@
 
 class Finance {
     //2D Arrays
-    String[][] antIncomeArr;
-    String[][] accIncomeArr;
-    String[][] accExpenseArr;
-    String[][] antExpenseArr;
+    public String[][] antIncomeArr;
+    public String[][] accIncomeArr;
+    public String[][] accExpenseArr;
+    public String[][] antExpenseArr;
+
+    
 
     //Methods
-
+    public void populateCategories(String[] arr, String[][] newArr){        
+        String[] categoryArr = {"Food", "Health", "Transportation", "Utilies", "Personal","Other"};
+        
+        for(int i = 0; i < 6; i++){
+            newArr[0][i] = categoryArr[i];
+            newArr[1][i] = arr[i]; // the single arrays from the gui
+        }        
+    }
     //insert importing method name (file io)
     //insert exporting method name
     //insert creating csv name
