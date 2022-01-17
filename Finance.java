23aac4324f78a2846d0a3a9f9e6944d6ccb222e9
rxchxl <95245860+rxchxl@users.noreply.github.com>
2022-01-17 14:11:04 -0500

    Add files via upload

--- Finance.java
+++ Finance.java
@@ -1,45 +1,50 @@
 /**
  * Base Class - Model - Finance
  */
+import java.io.*;
 
 class Finance {
-    //2D Arrays
-    public String[][] antIncomeArr = new String[6][2];
-    public String[][] accIncomeArr = new String[6][2];
-    public String[][] accExpenseArr = new String[6][2];
-    public String[][] antExpenseArr = new String[6][2];
+    
+
+    public int arraySize = 12;
+    public String directory = "C:/Users/Rachel/Downloads/Comp Sci/5. Culminating/BudjetingApp/src/";
+    public String csvName = "antIncome.csv";
 
     public Finance(){
 
     }
-
-    //Methods
-    public void populate(String[] arr, String[][] income, String[][] expenses){  
-        String[] incomeCat = {"Savings", "Paycheck", "Bonus", "Interest", "Allowance", "Other"};      
-        String[] expenseCat = {"Food", "Health", "Transportation", "Utilies", "Personal","Other"};
-        
-        int a = 0;
-        //populate categories
-        for(int i = 0; i < 12; i++){  
-            if(i < 6){
-                income[i][0] = incomeCat[i];  
-                expenses[i][0] = expenseCat[i];  
-                income[i][1] = arr[i];  
+    
+    public String[][] readCSV(String csvFile){
+        String[][] tempArray = new String[arraySize][2];
+        int i = 0;
+        try{
+            File file = new File(csvFile);
+            FileReader reader = new FileReader(file);
+            BufferedReader buffer = new BufferedReader(reader);
+            String line = "";
+            while((line = buffer.readLine()) != null) {
+                String[] arr = line.split(",");
+                tempArray[i][0] = arr[0];
+                if(arr.length > 1){
+                    tempArray[i][1] = arr[1];
+                }
+                i++;
             }
-            else{
-                expenses[a][1] = arr[i]; 
-                a++;  
-            }       
-        } 
-
-        //testing populate() 
-        for(int i = 0; i < 6; i++){
+            buffer.close();
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return tempArray;
+    }
+    //Method to test "readCSV()" method
+    public void test(){
+        String[][] arr = readCSV(this.directory + "antExpenses.csv");
+        for(int i = 0; i < 7; i++){
             for(int j = 0; j < 2; j++){
-                System.out.print(antExpenseArr[i][j] + ", ");
-            } 
+                System.out.print(arr[i][j] + ",");
+            }
             System.out.println();
         }
-
     }
     //insert importing method name (file io)
     //insert exporting method name
