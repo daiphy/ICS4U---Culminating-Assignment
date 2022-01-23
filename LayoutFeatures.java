7c00d471e6b378c43515f9f440373f1b8393eb70
daiphy <349857383@gapps.yrdsb.ca>
2022-01-23 13:19:11 -0500

    merged

--- LayoutFeatures.java
+++ LayoutFeatures.java
@@ -18,6 +18,7 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
+import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos; 
 import javafx.scene.image.Image;
@@ -186,6 +187,53 @@ public class LayoutFeatures{
         
         return incomeCat;
     }
+
+    // public ComboBox comboBoxIncome(){        
+            
+    //     // for(int i = 0; i < trends.income2D.length; i++){
+    //     //     for(int j = 0; j < 2; j++){
+    //     //         System.out.print(trends.income2D[i][j] + ", ");
+                
+    //     //     } 
+    //     //     System.out.println();
+    //     // }
+    //     // String temp = trends.income2D[1][0];
+    //     ComboBox incomeCat = new ComboBox();        
+    //     // // if(income2D[1][0]  null){
+    //     //     incomeCat.getItems().addAll(
+    //     //         temp 
+            
+    //     //     // "Savings", "Paycheck", "Bonus",
+    //     //         // "Interest", "Allowance", "Other"
+    //     //        );
+        
+    //     // // else{
+    //     //     System.out.println("entering their categories");
+
+
+    //         // for(int i = 1; i < income2D.length; i++){                
+    //                 // incomeCat.getItems().addAll(
+    //                 //     income2D[i][0]
+    //                 // );
+                
+    //         // }
+            
+    //     // }
+
+    //     if (tempArrayList.isEmpty() == true){
+    //         incomeCat.getItems().addAll("EMPTY");
+    //     }
+    //     else{
+    //         incomeCat.setItems(FXCollections
+    //         .observableArrayList(tempArrayList));
+    //     }
+        
+    //     incomeCat.setPromptText("Select Category");
+    //     incomeCat.setEditable(false);
+        
+    //     return incomeCat;
+    // }
+
     public ComboBox comboBoxExpense(){
         ComboBox expenseCat = new ComboBox();
         expenseCat.getItems().addAll(
