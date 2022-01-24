7bee9510e53a31cab7c73873253bd48888f9243d
daiphy <349857383@gapps.yrdsb.ca>
2022-01-24 03:17:18 -0500

    switch

--- LayoutFeatures.java
+++ LayoutFeatures.java
@@ -165,14 +165,14 @@ public class LayoutFeatures{
 
         return sPane;
     }
-    public ComboBox comboBoxMonths(String text){
+    public ComboBox comboBoxMonths(){
         ComboBox months = new ComboBox();
         months.getItems().addAll(
                                  "January", "February", "March", "April", "May", "June",
                                  "July", "August", "September", "October", "November", "December"
                                 );
         
-        months.setPromptText(text);
+        months.setPromptText("Select Month");
         months.setEditable(false);
         
         return months;
