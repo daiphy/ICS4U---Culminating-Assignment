210a3461ccd1a048c0327e6773f9d2b824931e6d
Jane Low <75858309+blessbtsss@users.noreply.github.com>
2022-01-25 00:53:37 -0500

    Add files via upload

--- Trends.java
+++ Trends.java
@@ -2,167 +2,186 @@ import java.util.ArrayList;
 import javafx.scene.control.ComboBox;
 
 public class Trends {
-    
-    //-------------------- GLOBAL VARIABLES --------------------//
-    //2D Arrays
-    public String[][] income2D = new String[7][4]; // it is initialized first so that it can be passed through to methods, but it's size changes depending on how many categories that the user enters
+
+    // -------------------- GLOBAL VARIABLES --------------------//
+    // 2D Arrays which store anticipated, actual, and difference data
+    public String[][] income2D = new String[7][4]; // Set so that the user can add up to six categories
     public String[][] expense2D = new String[7][4];
-    
+
     // Arraylists for scene 2, what the user enters is temporarily saved here
     public ArrayList<String> incomeCatList = new ArrayList<>();
     public ArrayList<String> expenseCatList = new ArrayList<>();
 
     // Default income and expense categories
-    public String[] defaultInc = {"Savings", "Paycheck", "Bonus", "Interest", "Allowance", "Other"};      // the space in front makes it line up with the num of rows in the 2d arrays
-    public String[] defaultExp = {"Food", "Health", "Transportation", "Utilies", "Personal","Other"}; // we need to make this permanent in the 2d arrays first or something
+    public String[] defaultInc = { "Savings", "Paycheck", "Bonus", "Interest", "Allowance", "Other" }; // the space in
+                                                                                                       // front makes it
+                                                                                                       // line up with
+                                                                                                       // the num of
+                                                                                                       // rows in the 2d
+                                                                                                       // arrays
+    public String[] defaultExp = { "Food", "Health", "Transportation", "Utilies", "Personal", "Other" }; // we need to
+                                                                                                         // make this
+                                                                                                         // permanent in
+                                                                                                         // the 2d
+                                                                                                         // arrays first
+                                                                                                         // or something
 
-    public String[] monthNames = {"January", "February", "March", "April", "May", 
-    "June", "July", "August", "September", "October", "November", "December"};
-    
-    public int totalAntIncome;
-    public int totalAntExpenses;
+    // Contains all the month names in a year
+    public String[] monthNames = { "January", "February", "March", "April", "May",
+            "June", "July", "August", "September", "October", "November", "December" };
 
-    public int totalAccIncome;
-    public int totalAccExpense;
-
-    // public FinanceFV financeFV = new FinanceFV();
-
-    //-------------------- CONSTRUCTOR --------------------//
-    public Trends(){
-
+    // -------------------- CONSTRUCTOR --------------------//
+    public Trends() {
     }
 
-    //Methods
-    
-    public ArrayList<String> defaultCategories(ArrayList<String> categoryArrList, String[] defaultCat){ //if they entered nothing, then populate the user's arraylist with default categories
-        
-            if(categoryArrList.isEmpty()){
-                for(int i = 0; i < defaultCat.length; i ++){ //starts at one because 0 is the month name
-                    categoryArrList.add(defaultCat[i]); // populate the categories with default if they entered nothing
-                }    
+    // -------------------- POPULATE USER ENTRIES ARRAYLIST WITH DEFAULT CATEGORIES
+    // --------------------//
+    /**
+     * Changes the user's entries arraylists to the default categories
+     * 
+     * @param categoryArrList the user's entries arraylist (can be income or
+     *                        expense)
+     * @param defaultCat      the array of the default categories (can be income or
+     *                        expense)
+     * @return the updated version of the user's entries arraylist
+     */
+    public ArrayList<String> defaultCategories(ArrayList<String> categoryArrList, String[] defaultCat) {
+
+        if (categoryArrList.isEmpty()) { // if they entered nothing, then populate the user's arraylist with default
+                                         // categories
+            for (int i = 0; i < defaultCat.length; i++) { // starts at one because 0 is the month name
+                categoryArrList.add(defaultCat[i]); // populate the categories with default if they entered nothing
             }
-            
-            return categoryArrList;
+        }
+        return categoryArrList;
     }
 
-    public String[][] populateCat(ArrayList<String> categoryArrList, String[][] twoDArr, String month){
-        // twoDArr = new String[categoryArrList.size()+ 1][4];  
-        
-        int addEmpty = twoDArr.length - categoryArrList.size(); // 7 - size of arrlist
-        for(int i = 0; i < addEmpty; i++){
+    // -------------------- POPULATE 2D ARRAY WITH CATEGORIES METHOD --------------------//
+    /**
+     * Populates the first column of the 2D array so that all the categories are
+     * stored
+     * 
+     * @param categoryArrList the user's entries arraylist
+     * @param twoDArr         the 2D array that stores user data (can be income or
+     *                        expense)
+     * @param month           the month that the user wants to work on
+     * @return the updated version of the 2D array
+     */
+    public String[][] populateCat(ArrayList<String> categoryArrList, String[][] twoDArr, String month) {
+
+        int addEmpty = twoDArr.length - categoryArrList.size(); // number of empty spaces we need to add to the
+                                                                // arraylist (so that it can match the length of the 2D
+                                                                // arrays)
+        for (int i = 0; i < addEmpty; i++) { // add the empty spaces
             categoryArrList.add("");
         }
-        twoDArr[0][0] = "Month: " + month; // Add the month to the 2d array
-        // System.out.println()
-        int a = 0;
-        for(int i = 1; i < twoDArr.length; i ++){ //starts at one because 0 is the month name
-            twoDArr[i][0] = categoryArrList.get(a);
-            a++;
-        }                
 
-        // for(int i = 0; i < )
-        
+        twoDArr[0][0] = "Month: " + month; // Add the month to the 2d array (will always be [0][0] of any 2D array)
+
+        int a = 0; // counter for the arraylist since its one index less than 2D array
+        for (int i = 1; i < twoDArr.length; i++) { // starts at one because 0 is the month name
+            twoDArr[i][0] = categoryArrList.get(a); // populates the first column of the 2D array
+            a++;
+        }
         return twoDArr;
     }
-    
-    public String[][] populate(ArrayList<String> catArr, ArrayList<String> amtArr, String[][] twoDArr, ArrayList<String> labelCat, int col, String month){ //CHANGE THE INCOME NAMES                                 
-        
-        int a = 0;
-        for(int i = 1; i < twoDArr.length; i++){
-            String temp = labelCat.get(a); 
+
+    // -------------------- POPULATE ANTICIPATED AND ACTUAL COLUMNS OF 2D ARRAY METHOD --------------------//
+    /**
+     * Populates the anticipated and actual columns of the 2D arrays from what the
+     * user entered.
+     * Since the user can enter a specific category with an amount($) several times,
+     * this method takes the totals of the amount and
+     * stores in all into one place
+     * 
+     * @param catArr   arraylist that holds all the categories that the user has
+     *                 entered (can be for income or expense)
+     * @param amtArr   arraylist that holds all the amounts that the user has
+     *                 entered (can be for income or expense)
+     * @param twoDArr  the 2D array that stores user data (can be income or expense)
+     * @param labelCat the arraylist that has all of the categories (no duplicates,
+     *                 the one we reference off of)
+     * @param col      the index number of the column we want to store to (because
+     *                 anticipated and actual are in different columns)
+     * @param month    the month that the user wants to work on
+     * @return the updated version of the 2D array (holds categories, anticipated
+     *         and actual)
+     */
+    public String[][] populate(ArrayList<String> catArr, ArrayList<String> amtArr, String[][] twoDArr,
+            ArrayList<String> labelCat, int col, String month) { // CHANGE THE INCOME NAMES
+
+        int a = 0; // counter for the arraylist since its one index less than 2D array
+        for (int i = 1; i < twoDArr.length; i++) { // loops through the 2D array with just the values and not the month
+                                                   // name
+            String temp = labelCat.get(a);
             Double addOn = 0.0;
-            for(int j = 0; j < catArr.size(); j++){
-                if(!amtArr.get(j).isEmpty() && catArr.get(j) != null){
-                    if(catArr.get(j).equals(temp)){
+            for (int j = 0; j < catArr.size(); j++) { // loops through the size of the catArr because the user can enter
+                                                      // many different amounts per category
+                if (!amtArr.get(j).isEmpty() && catArr.get(j) != null) { // if the amount and category arrays are not
+                                                                         // empty/null, and the category array is
+                                                                         // aligned with
+                    if (catArr.get(j).equals(temp)) { // the category label list, then add the values of the amount
+                                                      // together (this gets the total)
                         addOn += Double.parseDouble(amtArr.get(j));
-                   }                
-                }   
-            }        
-            a++;    
-            
-            twoDArr[i][col] = String.valueOf(addOn);            
-        }
-        System.out.println("before chaning to 0.0");
-        for(int i = 0; i < twoDArr.length; i++){
-            for(int j = 0; j < twoDArr[0].length; j++){
-                System.out.print(twoDArr[i][j] + ", ");
-                
-            } 
-            System.out.println();
-        }
-        for(int i = 0; i < twoDArr.length; i++){
-            if (twoDArr[i][1] == null || twoDArr[i][1].equals("")){
-                twoDArr[i][1] = "0.0";                
+                    }
+                }
             }
-            if (twoDArr[i][2] == null){
+            a++;
+
+            twoDArr[i][col] = String.valueOf(addOn); // this populates the 2D array with the calculated sum of the
+                                                     // entries for anticipated/actual
+        }
+
+        for (int i = 0; i < twoDArr.length; i++) { // looping through the entire array, if it's null or empty then
+                                                   // replace that element with 0.0
+            if (twoDArr[i][1] == null || twoDArr[i][1].equals("")) {
+                twoDArr[i][1] = "0.0";
+            }
+            if (twoDArr[i][2] == null) {
                 twoDArr[i][2] = "0.0";
             }
-            if (twoDArr[i][3] == null){
+            if (twoDArr[i][3] == null) {
                 twoDArr[i][3] = "0.0";
             }
-        }           
-        for(int i = 0; i < twoDArr.length; i++){
-            for(int j = 0; j < twoDArr[0].length; j++){
-                System.out.print(twoDArr[i][j] + ", ");
-                
-            } 
-            System.out.println();
         }
+
         return twoDArr;
     }
 
-    //calculates the difference between two doubles
-    //can use this to find the difference between acc and ant income/expense, total accIncome - total accExpense
-    public String[][] populateDiff(String[][] twoDArr){        
-        
-        System.out.println("in diff");
-        for(int i = 0; i < twoDArr.length; i++){
-            for(int j = 0; j < twoDArr[0].length; j++){
-                System.out.print(twoDArr[i][j] + ", ");
-                
-            } 
-            System.out.println();
-        }
-        
-        for(int i = 1; i < twoDArr.length; i++){
+    // -------------------- POPULATE 2D ARRAY WITH CALCULATED DIFFERENCE METHOD --------------------//
+    /**
+     * This method calculates the difference between two doubles then
+     * populates the calculated differences into a 2D array
+     * 
+     * @param twoDArr this is the 2D array that the data is being stored in
+     * @return this method will return the 2D array with the updated data
+     */
+    public String[][] populateDiff(String[][] twoDArr) {
+        for (int i = 1; i < twoDArr.length; i++) {
+            // initialize a double for anticipated and actual income/expense
             double ant = Double.parseDouble(twoDArr[i][1]);
-            double acc = Double.parseDouble(twoDArr[i][2]);                        
-            double diff = ant-acc;
-            twoDArr[i][3] = Double.toString(diff);            
+            double acc = Double.parseDouble(twoDArr[i][2]);
+            double diff = ant - acc; // calculate the difference between two doubles
+            twoDArr[i][3] = Double.toString(diff); // populate the 2D array with the calculated difference
         }
         return twoDArr;
     }
-
-    //Calculates the sum of the amounts column in a two d array
-    public double sumCalculator(String[][] twoDArr){
-        double addTerm;
-        double sum = 0;
-
-        for(int i = 1; i < twoDArr.length; i++){
-            addTerm = Double.valueOf(twoDArr[i][1]);
+    // -------------------- CALCULATES SUM OF AMOUNTS COLUMN METHOD --------------------//
+    /**
+     * Calculates the sum of the amounts columns in the 2D array (anticipated or actual)
+     * @param twoDArr the 2D array that stores user data (can be income or expense)                   
+     * @param col the index number of the column for the 2D array
+     * @return the sum of the amounts
+     */
+    public double sumCalculator(String[][] twoDArr, int col) {
+        // Initialize doubles
+        double addTerm; // value to add to the sum
+        double sum = 0; // stores the sum of added values        
+        
+        for (int i = 1; i < twoDArr.length; i++) {    // Loops through a 2D array (either the income or expense array)
+            addTerm = Double.valueOf(twoDArr[i][1]);  // Adds the values of the anticipated income/expense
             sum += addTerm;
         }
-        
         return sum;
     }    
-    
-    //Calculates the sum of the amounts column in a two d array
-    public double sumAccCalculator(String[][] twoDArr){
-        double addTerm;
-        double sum = 0;
-
-        for(int i = 1; i < twoDArr.length; i++){
-            addTerm = Double.valueOf(twoDArr[i][2]);
-            sum += addTerm;
-        }
-        
-        return sum;
-    }
-
-    //calculates the end balance
-    public double endBalance(double startingBalance, double accSavings){
-        return startingBalance + accSavings;
-    }
-    //TO DO:
-    // -    add all the totals in acc income and expense by using arraylists and cross references with category names to enter into 2d array
 }
