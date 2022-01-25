d08981fbe09bdbb46842fedb136e638aa554de37
Hilary Sze <95245546+hilarysze@users.noreply.github.com>
2022-01-25 00:35:39 -0500

    Add files via upload

--- Trends.java
+++ Trends.java
@@ -2,164 +2,171 @@ import java.util.ArrayList;
 import javafx.scene.control.ComboBox;
 
 public class Trends {
-    
-    //-------------------- GLOBAL VARIABLES --------------------//
-    //2D Arrays
-    public String[][] income2D = new String[7][4]; // temporary initialize
-    public String[][] expense2D = new String[7][4];
-    
-    public ArrayList<String> incomeCatList = new ArrayList<>();
-    public ArrayList<String> expenseCatList = new ArrayList<>();
-    public String[] defaultInc = {"Savings", "Paycheck", "Bonus", "Interest", "Allowance", "Other"};      // the space in front makes it line up with the num of rows in the 2d arrays
-    public String[] defaultExp = {"Food", "Health", "Transportation", "Utilies", "Personal","Other"}; // we need to make this permanent in the 2d arrays first or something
-
-    public String[] monthNames = {"January", "February", "March", "April", "May", 
+  
+  //-------------------- GLOBAL VARIABLES --------------------//
+  //2D Arrays
+  public String[][] income2D = new String[7][4]; // it is initialized first so that it can be passed through to methods, but it's size changes depending on how many categories that the user enters
+  public String[][] expense2D = new String[7][4];
+  
+  // Arraylists for scene 2, what the user enters is temporarily saved here
+  public ArrayList<String> incomeCatList = new ArrayList<>();
+  public ArrayList<String> expenseCatList = new ArrayList<>();
+  
+  // Default income and expense categories
+  public String[] defaultInc = {"Savings", "Paycheck", "Bonus", "Interest", "Allowance", "Other"};      // the space in front makes it line up with the num of rows in the 2d arrays
+  public String[] defaultExp = {"Food", "Health", "Transportation", "Utilies", "Personal","Other"}; // we need to make this permanent in the 2d arrays first or something
+  
+  public String[] monthNames = {"January", "February", "March", "April", "May", 
     "June", "July", "August", "September", "October", "November", "December"};
+  
+  public int totalAntIncome;
+  public int totalAntExpenses;
+  
+  public int totalAccIncome;
+  public int totalAccExpense;
+  
+  // public FinanceFV financeFV = new FinanceFV();
+  
+  //-------------------- CONSTRUCTOR --------------------//
+  public Trends(){
     
-    public int totalAntIncome;
-    public int totalAntExpenses;
-
-    public int totalAccIncome;
-    public int totalAccExpense;
-
-    // public FinanceFV financeFV = new FinanceFV();
-
-    //-------------------- CONSTRUCTOR --------------------//
-    public Trends(){
-
-    }
-
-    //Methods
+  }
+  
+  //Methods
+  
+  public ArrayList<String> defaultCategories(ArrayList<String> categoryArrList, String[] defaultCat){ //if they entered nothing, then populate the user's arraylist with default categories
     
-    public ArrayList<String> defaultCategories(ArrayList<String> categoryArrList, String[] defaultCat){ //if they entered nothing, then populate the user's arraylist with default categories
-        
-            if(categoryArrList.isEmpty()){
-                for(int i = 0; i < defaultCat.length; i ++){ //starts at one because 0 is the month name
-                    categoryArrList.add(defaultCat[i]); // populate the categories with default if they entered nothing
-                }    
-            }
-            
-            return categoryArrList;
-    }
-
-    public String[][] populateCat(ArrayList<String> categoryArrList, String[][] twoDArr, String month){
-        twoDArr = new String[categoryArrList.size()+ 1][4];  
-        
-        
-        twoDArr[0][0] = "Month: " + month; // Add the month to the 2d array
-
-
-        int a = 0;
-        for(int i = 1; i < twoDArr.length; i ++){ //starts at one because 0 is the month name
-            twoDArr[i][0] = categoryArrList.get(a);
-            a++;
-        }                
-        
-        return twoDArr;
+    if(categoryArrList.isEmpty()){
+      for(int i = 0; i < defaultCat.length; i ++){ //starts at one because 0 is the month name
+        categoryArrList.add(defaultCat[i]); // populate the categories with default if they entered nothing
+      }    
     }
     
-    public String[][] populate(ArrayList<String> catArr, ArrayList<String> amtArr, String[][] twoDArr, ArrayList<String> labelCat, int col, String month){ //CHANGE THE INCOME NAMES                                 
-        
-        int a = 0;
-        for(int i = 1; i < twoDArr.length; i++){
-            String temp = labelCat.get(a); 
-            Double addOn = 0.0;
-            for(int j = 0; j < catArr.size(); j++){
-                if(!amtArr.get(j).isEmpty() && catArr.get(j) != null){
-                    if(catArr.get(j).equals(temp)){
-                        addOn += Double.parseDouble(amtArr.get(j));
-                   }                
-                }   
-            }        
-            a++;    
-            
-            twoDArr[i][col] = String.valueOf(addOn);
-
-        }
-        System.out.println("in populate");
-        for(int i = 0; i < twoDArr.length; i++){
-            for(int j = 0; j < 4; j++){
-                System.out.print(twoDArr[i][j] + ", ");
-                
-            } 
-            System.out.println();
-        }        
-        return twoDArr;
-    }
-
-    //calculates the difference between two doubles
-    //can use this to find the difference between acc and ant income/expense, total accIncome - total accExpense
-    public String[][] populateDiff(String[][] twoDArr){        
-
-        for(int i = 0; i < twoDArr.length; i++){
-            if (twoDArr[i][1] == null || twoDArr[i][1].equals("")){
-                System.out.println("no longer empty");
-                twoDArr[i][1] = "0.0";
-            }
-
-            if (twoDArr[i][2] == null){
-                twoDArr[i][2] = "0.0";
-            }
-            if (twoDArr[i][3] == null){
-                twoDArr[i][3] = "0.0";
-            }
-        }
-        System.out.println("in diff");
-        for(int i = 0; i < twoDArr.length; i++){
-            for(int j = 0; j < twoDArr[0].length; j++){
-                System.out.print(twoDArr[i][j] + ", ");
-                
-            } 
-            System.out.println();
-        }
-        
-        for(int i = 1; i < twoDArr.length; i++){
-            double ant = Double.parseDouble(twoDArr[i][1]);
-            double acc = Double.parseDouble(twoDArr[i][2]);                        
-            double diff = ant-acc;
-            twoDArr[i][3] = Double.toString(diff);            
-        }
-        for(int i = 0; i < twoDArr.length; i++){
-            for(int j = 0; j < twoDArr[0].length; j++){
-                System.out.print(twoDArr[i][j] + ", ");
-                
-            } 
-            System.out.println();
-        }      
-        return twoDArr;
-    }
-
-
-    //Calculates the sum of the amounts column in a two d array
-    public double sumCalculator(String[][] twoDArr){
-        double addTerm;
-        double sum = 0;
-
-        for(int i = 1; i < twoDArr.length; i++){
-            addTerm = Double.valueOf(twoDArr[i][1]);
-            sum += addTerm;
-        }
-        
-        return sum;
-    }    
+    return categoryArrList;
+  }
+  
+  public String[][] populateCat(ArrayList<String> categoryArrList, String[][] twoDArr, String month){
+    // twoDArr = new String[categoryArrList.size()+ 1][4];  
     
-    //Calculates the sum of the amounts column in a two d array
-    public double sumAccCalculator(String[][] twoDArr){
-        double addTerm;
-        double sum = 0;
-
-        for(int i = 1; i < twoDArr.length; i++){
-            addTerm = Double.valueOf(twoDArr[i][2]);
-            sum += addTerm;
-        }
+    int addEmpty = twoDArr.length - categoryArrList.size(); // 7 - size of arrlist
+    for(int i = 0; i < addEmpty; i++){
+      categoryArrList.add("");
+    }
+    twoDArr[0][0] = "Month: " + month; // Add the month to the 2d array
+    // System.out.println()
+    int a = 0;
+    for(int i = 1; i < twoDArr.length; i ++){ //starts at one because 0 is the month name
+      twoDArr[i][0] = categoryArrList.get(a);
+      a++;
+    }                
+    
+    // for(int i = 0; i < )
+    
+    return twoDArr;
+  }
+  
+  public String[][] populate(ArrayList<String> catArr, ArrayList<String> amtArr, String[][] twoDArr, ArrayList<String> labelCat, int col, String month){ //CHANGE THE INCOME NAMES                                 
+    
+    int a = 0;
+    for(int i = 1; i < twoDArr.length; i++){
+      String temp = labelCat.get(a); 
+      Double addOn = 0.0;
+      for(int j = 0; j < catArr.size(); j++){
+        if(!amtArr.get(j).isEmpty() && catArr.get(j) != null){
+          if(catArr.get(j).equals(temp)){
+            addOn += Double.parseDouble(amtArr.get(j));
+          }                
+        }   
+      }        
+      a++;    
+      
+      twoDArr[i][col] = String.valueOf(addOn);            
+    }
+    System.out.println("before chaning to 0.0");
+    for(int i = 0; i < twoDArr.length; i++){
+      for(int j = 0; j < twoDArr[0].length; j++){
+        System.out.print(twoDArr[i][j] + ", ");
         
-        return sum;
+      } 
+      System.out.println();
     }
-
-    //calculates the end balance
-    public double endBalance(double startingBalance, double accSavings){
-        return startingBalance + accSavings;
+    for(int i = 0; i < twoDArr.length; i++){
+      if (twoDArr[i][1] == null || twoDArr[i][1].equals("")){
+        twoDArr[i][1] = "0.0";                
+      }
+      if (twoDArr[i][2] == null){
+        twoDArr[i][2] = "0.0";
+      }
+      if (twoDArr[i][3] == null){
+        twoDArr[i][3] = "0.0";
+      }
+    }           
+    for(int i = 0; i < twoDArr.length; i++){
+      for(int j = 0; j < twoDArr[0].length; j++){
+        System.out.print(twoDArr[i][j] + ", ");
+        
+      } 
+      System.out.println();
     }
-    //TO DO:
-    // -    add all the totals in acc income and expense by using arraylists and cross references with category names to enter into 2d array
+    return twoDArr;
+  }
+  
+  //calculates the difference between two doubles
+  //can use this to find the difference between acc and ant income/expense, total accIncome - total accExpense
+  public String[][] populateDiff(String[][] twoDArr){        
+    
+    System.out.println("in diff");
+    for(int i = 0; i < twoDArr.length; i++){
+      for(int j = 0; j < twoDArr[0].length; j++){
+        System.out.print(twoDArr[i][j] + ", ");
+        
+      } 
+      System.out.println();
+    }
+    
+    for(int i = 1; i < twoDArr.length; i++){
+      double ant = Double.parseDouble(twoDArr[i][1]);
+      double acc = Double.parseDouble(twoDArr[i][2]);                        
+      double diff = ant-acc;
+      twoDArr[i][3] = Double.toString(diff);            
+    }
+    return twoDArr;
+  }
+  
+  
+  //Calculates the sum of the amounts column in a two d array
+  public double sumCalculator(String[][] twoDArr){
+    //Initialize doubles
+    double addTerm; //value to add to the sum
+    double sum = 0; //stores the sum of added values
+    
+    //Loops through a 2D array (either the income or expense array)
+    //Adds the values of the anticipated income/expense
+    for(int i = 1; i < twoDArr.length; i++){
+      addTerm = Double.valueOf(twoDArr[i][1]);
+      sum += addTerm;
+    }
+    return sum;
+  }    
+  
+  //Calculates the sum of the amounts column in a two d array
+  public double sumAccCalculator(String[][] twoDArr){
+    //Initialize doubles  
+    double addTerm; //value to add to the sum
+    double sum = 0; //stores the sum of added values
+    
+    //Loops through a 2D array (either the income or expense array)
+    //Adds the values of the actual income/expense
+    for(int i = 1; i < twoDArr.length; i++){
+      addTerm = Double.valueOf(twoDArr[i][2]);
+      sum += addTerm;
+    }
+    return sum;
+  }
+  
+  //Calculates the end balance
+  public double endBalance(double startingBalance, double accSavings){
+    //Adds the starting/original balance and the actual savings in a given month
+    return startingBalance + accSavings;
+  }
 }
