3fb1efad6557a73b478681ac00e8c31e7469052f
daiphy <349857383@gapps.yrdsb.ca>
2022-01-24 19:55:01 -0500

    spacing

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ public class Main{
     public static void main(String[] args){
         //initializing the view
         AppLayoutFV gui = new AppLayoutFV();
-        gui.launch(AppLayoutFV.class, args);
-
+        gui.launch(AppLayoutFV.class, args); 
     }
 }
