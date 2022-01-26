7c896a73c9d4f244936ce9aecb431a1377b53763
daiphy <349857383@gapps.yrdsb.ca>
2022-01-25 20:02:32 -0500

    fixed

--- Main.java
+++ Main.java
@@ -1,8 +1,17 @@
+/**
+ * Daiphy, Hilary, Jane, Rachel
+ * Teacher: Mr. Ho
+ * ICS4U Culminating Project
+ * January 24, 2022
+ * Main.java
+ */
+
+//********** CLASS **********//
 public class Main{
     public static void main(String[] args){
         //initializing the view
         AppLayoutFV gui = new AppLayoutFV();
-        gui.launch(AppLayoutFV.class, args);
+        gui.launch(AppLayoutFV.class, args);    //launching the view
 
     }
 }
